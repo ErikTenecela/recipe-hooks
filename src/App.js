18959f0030667911aed9aa903d328bd00a3a9470
Erik Tenecela <eriktenecela13@gmail.com>
2020-02-07 17:19:27 -0500

    full-working

--- src/App.js
+++ src/App.js
@@ -6,21 +6,38 @@ function App() {
   const APP_ID = "4b03ac4f";
   const APP_KEY = "24847092d6fba21f22e46d0e264662f4";
   const [store, setStore] = useState([]);
+  const [query, setQuery] = useState("chicken");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     Axios.get(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     )
       .then(res => {
         console.log(res.data);
         setStore(res.data.hits);
       })
       .catch(e => console.log(e.message));
-  }, []);
+  }, [query]);
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+  };
+  //onSubmit runs only when we have a forum wrapped around the fourm element
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
         <button className="search-button" type="submit">
           Search
         </button>
@@ -31,6 +48,7 @@ function App() {
           calories={each.recipe.calories}
           img={each.recipe.image}
           key={each.recipe.label}
+          ingredients={each.recipe.ingredients}
         />
       ))}
     </div>
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-function Recipe({ key, title, calories, img }) {
+function Recipe({ key, title, calories, img, ingredients }) {
   return (
     <div>
       <h1 key={key}>{title}</h1>
       <p>{calories}</p>
       <img src={img} alt="" />
+      <ol>
+        {ingredients.map(each => (
+          <li>
+            Instruction: {each.text} Wieght: {each.weight}
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
